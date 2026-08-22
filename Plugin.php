87b3adcfd215cc6ab02ec8bc12863652215a48bc
Carmelo Santana <me@carmelosantana.com>
2026-08-22 09:16:29 -0400

    chore: bump version to 1.1.0

--- Plugin.php
+++ Plugin.php
@@ -76,7 +76,7 @@ class Plugin extends Base
 
     public function getPluginVersion(): string
     {
-        return '1.0.0';
+        return '1.1.0';
     }
 
     public function getPluginHomepage(): string
